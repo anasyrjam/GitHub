132143d36c67569a7cc464b4af118fa6ee0b291c
= <=>
2018-12-23 11:12:55 +0200

    third commit

--- src/github/Github.java
+++ src/github/Github.java
@@ -15,7 +15,7 @@ public class Github {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("welcome");
+        System.out.println("Welcome to java");
     }
     
 }
